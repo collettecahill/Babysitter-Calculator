96e4756e0dc83f312dee375bb57beae863ada179
collettecahill <collette@pcdlondon.com>
2023-05-10 16:47:58 +0100

    Code tidy up

--- script.js
+++ script.js
@@ -1,31 +1,40 @@
+
+//Capture the value for age entry inputs  
+
 function genAgeValue() {
-    const childrenInput = document.getElementById("number-of-children");
-    const noOfElements = childrenInput.value;
-    console.log("No of Elements", noOfElements);
+    let childrenInput = document.getElementById("X")
+    let noOfElements = childrenInput.value
+    console.log(noOfElements)
+    if (noOfElements >= 2) {
+        let newElement = document.getElementById("input-age-of-children")
+        newElement.insertAdjacentHTML("afterend", "<p>test</p>")
     }
+}
+
+//Create age entry input for 'age of children' 
 
 function createNewElement() {
-    /*const addLabel = document.createElement("label");
-    addLabel.for="age-of-children";
-    addLabel.textContent="Age of Children";
-    const addInput = document.createElement("input");
-    addInput.type = "number";
-    addInput.idName = "age-of-children"; 
-    addInput.name = "age-of-children";
-    addInput.required;
-    document.getElementById("age-of-children").appendChild(addLabel, addInput);
-    ;  // entered value for number of children*/
+    let addLabel = document.createElement("label")
+    addLabel.for="age-of-children"
+    addLabel.textContent="Age of Children"
+    let addInput = document.createElement("input")
+    addInput.type = "number"
+    addInput.idName = "age-of-children" 
+    addInput.required
 
-    /*USE A FOR LOOP TO CREATE MULTIPLE ELEMENTS*/
-
-    const paraMsg = document.getElementById("age-of-children");
-    paraMsg.insertAdjacentHTML ("afterend", "<p>Hi Again!</p>"); 
+    document.getElementById("age-of-children").appendChild(addLabel, addInput)
+    
 }
-function repeatElementTimesValue() {
 
+
+//Create multiple age entry inputs based on value
+
+
+
+//Calculate hourly rate
+
+function calcPrice() {
+    const childrenInput = document.getElementById("number-of-children")
+    const totalPrice = document.getElementById("total-price")
+    totalPrice.textContent = "£" + (childrenInput.value*10).toFixed(2)
 }
-/*function calcPrice() {
-    const childrenInput = document.getElementById("number-of-children");
-    const totalPrice = document.getElementById("total-price");
-    totalPrice.textContent = "£" + (childrenInput.value*10).toFixed(2);}*/
-       
